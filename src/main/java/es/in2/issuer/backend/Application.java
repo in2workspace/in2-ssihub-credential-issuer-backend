7ebfb18aec2f4517f6b73234db750383a3a14660
Oriol Canadés <oriol.canades@in2.es>
2025-02-06 12:48:54 +0100

    Initial commit

---
+++ src/main/java/es/in2/issuer/backend/Application.java
@@ -0,0 +1,13 @@
+package es.in2.issuer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
