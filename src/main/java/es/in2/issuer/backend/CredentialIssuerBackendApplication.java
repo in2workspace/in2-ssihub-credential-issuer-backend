073206890a95634320f5b5ad6aad0b1e35f15598
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2025-02-23 11:33:17 +0100

    New Backend parent project New SonarQube Cloud configuration

---
+++ src/main/java/es/in2/issuer/backend/CredentialIssuerBackendApplication.java
@@ -0,0 +1,13 @@
+package es.in2.issuer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialIssuerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredentialIssuerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/in2/issuer/backend/CredentialIssuerBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package es.in2.issuer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialIssuerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test context load
+	}
+
+}
